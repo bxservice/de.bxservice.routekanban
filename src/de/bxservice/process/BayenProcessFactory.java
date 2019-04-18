fcad02d8403afe707ef45134bff94ea0f64a1992
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2019-04-18 18:29:38 +0200

    Processes adapted to kanban 6.2

--- src/de/bxservice/process/BayenProcessFactory.java
+++ src/de/bxservice/process/BayenProcessFactory.java
@@ -9,12 +9,8 @@ public class BayenProcessFactory implements IProcessFactory {
 	public ProcessCall newProcessInstance(String className) {
 		if (BayenPrintInvoiceProcess.class.getName().equals(className))
 			return new BayenPrintInvoiceProcess();
-		if (BayenPrintInvoiceTomorrow.class.getName().equals(className))
-			return new BayenPrintInvoiceTomorrow();
 		if (BayenLoadingTourPrint.class.getName().equals(className))
 			return new BayenLoadingTourPrint();
-		if (BayenLoadingTourPrintTomorrow.class.getName().equals(className))
-			return new BayenLoadingTourPrintTomorrow();
 		
 		return null;
 	}
